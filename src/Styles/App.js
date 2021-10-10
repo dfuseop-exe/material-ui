0d44e0f82e6b28263c08baf634f1da0e912e722b
dfuseop-exe <sushant.165.shinde@gmail.com>
2021-10-10 15:44:14 +0530

    makestyles

--- src/Styles/App.js
+++ src/Styles/App.js
@@ -1,15 +1,25 @@
 import "../Styles/App.css";
 import React from "react";
 import Typography from "@mui/material/Typography";
-import { Box } from "@mui/material";
+import { makeStyles } from '@material-ui/core/styles'
+
+
+const useStyles = makeStyles({
+  pink : {
+    color : "deeppink" ,
+   fontSize : 150 ,
+  },
+});
 
 function App() {
+
+  const classes = useStyles();
   return (
     <>
-      <Typography variant="h3" align="center" color="secondary" gutterBottom>Hello Material UI</Typography>
+      <Typography variant="h3" align="center" className={classes.pink} gutterBottom>Hello Material UI</Typography>
       {/* <Typography variant="h3" align="right">Hello Material UI</Typography> */}
 
-      <Typography variant="subtitle2" align="justify" color="primary">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore iste, ratione incidunt blanditiis laborum, obcaecati accusantium vitae corrupti possimus quo amet, aliquid omnis reprehenderit dolorum veritatis ut animi minima sequi? Lorem ipsum, amet consectetur adipisicing elit. Temporibus eligendi vel quas beatae repellat in illum debitis perspiciatis maiores commodi? Pariatur magni distinctio placeat quis tempora eos neque possimus animi?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore iste, ratione incidunt blanditiis laborum, obcaecati accusant dolor sit amet consectetur adipisicing elit. Temporibus eligendi vel quas beatae repellat in illum debitis perspiciatis ium vitae corrupti possimus quo amet, aliquid omnis reprehenderit dolorum veritatis ut animi minima sequi? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus eligendi vel quas beatae repellat in illum debitis perspiciatis maiores commodi? Pariatur magni distinctio placeat quis tempora eos neque possimus animi?</Typography>
+      
     </>
   );
 }
